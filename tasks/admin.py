c2300ba571e9d4389102cc48cd982ee94b077da0
LuisHb211 <luishbsantiago@gmail.com>
2025-05-19 10:12:57 -0300

    admin.py created

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import TaskStatus, Task
+
+@admin.register(TaskStatus)
+class TaskStatusAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    
+        
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'created_at', 'finish_at', 'status']
--- tasks/models.py
+++ tasks/models.py
@@ -2,14 +2,12 @@ from django.db import models
 
 
 class TaskStatus(models.Model):
-	status_id = models.AutoField(primary_key=True)
 	name = models.CharField(max_length=65)
  
 	def __str__(self):
 		return self.name
 
-class Tasks(models.Model):
-	task_id = models.AutoField(primary_key=True)
+class Task(models.Model):
 	title = models.CharField(max_length=65, verbose_name=('Title'))
 	description = models.CharField(max_length=165)
 	created_at = models.DateTimeField(auto_now_add=True)
