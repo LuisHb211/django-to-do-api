9bea4a43246fc5f742c85f182e9abe10efda95e2
LuisHb211 <luishbsantiago@gmail.com>
2025-05-19 10:02:24 -0300

    models created

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class TaskStatus(models.Model):
+	status_id = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=65)
+ 
+	def __str__(self):
+		return self.name
+
+class Tasks(models.Model):
+	task_id = models.AutoField(primary_key=True)
+	title = models.CharField(max_length=65, verbose_name=('Title'))
+	description = models.CharField(max_length=165)
+	created_at = models.DateTimeField(auto_now_add=True)
+	finish_at = models.DateField()
+	status = models.ForeignKey(
+		TaskStatus, on_delete=models.SET_NULL, null=True, blank=True, default=None
+	)
+ 
+	def __str__(self):
+		return self.title
+	
