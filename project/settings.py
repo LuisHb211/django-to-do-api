bd3a8869d7c3d544e5746eb3851f88e555473386
LuisHb211 <luishbsantiago@gmail.com>
2025-05-20 10:01:36 -0300

    Add paginaton to the API

--- project/settings.py
+++ project/settings.py
@@ -123,3 +123,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#It will change the pagination of all the api
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE':10,
+}
--- tasks/views.py
+++ tasks/views.py
@@ -4,10 +4,18 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from .models import Task
 from .serializers import TaskSerializer, TaskStatusSerializer
 
+from rest_framework.pagination import PageNumberPagination
+
+# This class was created to change the pagination just for the api
+class TaskPagination(PageNumberPagination):
+    page_size = 2
+
 class TaskAPIList(ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    pagination_class = TaskPagination
 
 class TaskAPIDetail(RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    pagination_class = TaskPagination
