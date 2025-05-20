8865402d1447b78bed9592f5fa5ad4f64750b3cf
LuisHb211 <luishbsantiago@gmail.com>
2025-05-20 09:39:20 -0300

    using the name of the status

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -7,7 +7,14 @@ class TaskStatusSerializer(serializers.ModelSerializer):
         fields = ['id', 'name']
 
 class TaskSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Task
-		fields = ['id', 'title', 'description', 'created_at', 'finish_at', 'status']
-	
+    class Meta:
+        model = Task
+        fields = ['id', 'title', 'description', 'created_at', 'finish_at', 'status', 'status_detail']
+
+    status_detail = TaskStatusSerializer(source='status', read_only=True)
+    
+    # This way I can pass the id of the status at PUT and UPDATE and still saw the name of the status in GET
+    # def to_representation(self, instance):
+    #     rep = super().to_representation(instance)
+    #     rep['status'] = instance.status.name if instance.status else None
+    #     return rep
