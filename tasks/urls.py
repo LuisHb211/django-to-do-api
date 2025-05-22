1e231d14e0276c4e20dc82477b0d7281a1f2e3f7
LuisHb211 <luishbsantiago@gmail.com>
2025-05-22 10:22:54 -0300

    Using drf simplejwt

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,12 +1,24 @@
-from django.urls import path
+from django.urls import path, include
 from tasks import views
 from rest_framework.routers import SimpleRouter
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from rest_framework_simplejwt.views import TokenVerifyView
 from .views import TaskAPIViewSet
 
 app_name = 'tasks'
 
-router = SimpleRouter()
-router.register("tasks", TaskAPIViewSet)
+task_api_router = SimpleRouter()
+task_api_router.register("tasks/api", TaskAPIViewSet, basename='tasks-api')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('tasks/api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('tasks/api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('tasks/api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),      
+
+    path('', include(task_api_router.urls))
+]
+
+
